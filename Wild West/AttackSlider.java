f584140a443a571d62d7e1fadc4aaa149c42b84e
BagelBro11 <mcdougalln@limestone.on.ca>
2023-06-11 17:46:55 -0400

    Bar Sliding
    attack slider bounces within the attack bar

--- Wild West/AttackSlider.java
+++ Wild West/AttackSlider.java
@@ -8,6 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class AttackSlider extends Actor
 {
+    int deltaX;
+    public AttackSlider(){
+        deltaX = 5;
+    }
     /**
      * Act - do whatever the AttackSlider wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +19,20 @@ public class AttackSlider extends Actor
     public void act()
     {
         // Add your action code here.
+        //if touching left side of attack bar go other way
+        if(getX() < 200){
+            //setLocation(getWorld().getWidth() /2,getWorld().getHeight() /2 );
+            deltaX = 5;
+            
+            
+        }
+        //if touching right side of attack bar go other way
+        if(getX() > 700){
+            //setLocation(getWorld().getWidth() /2,getWorld().getHeight() /2 );
+            deltaX = -5;
+            
+            
+        }
+        setLocation(getX() + deltaX, getY());
     }
 }
