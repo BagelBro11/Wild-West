3783149e8e3df7464655c3b1baf67771852ba5f6
SeanG428 <128709484+SeanG428@users.noreply.github.com>
2023-06-12 09:06:14 -0400

    running animation conditions

--- Wild West/PlayerBattleCharacter.java
+++ Wild West/PlayerBattleCharacter.java
@@ -9,25 +9,33 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class PlayerBattleCharacter extends Actor
 {
     boolean attack = false;
+    boolean inBattle = false;
+    int runAnimation = 0;
     /**
      * Act - do whatever the PlayerBattleCharacter wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Idle animation
-        if (attack == false) {
-
-        }
-        // Attack animation
-        else if (attack == true) {
-            // Draw weapon and shoot
-            for (int i = 1; i <= 22; i++) {
-                    
+        // Run animation
+        if (inBattle == false) {
+            runAnimation++;
+            // Change Image
+            
+            if (runAnimation == 4) {
+                runAnimation = 0;
             }
-            // Holster weapon
-            for (int i = 14; i > 0; i--) {
-                    
+        } else {
+            // Attack animation
+            if (attack == true) {
+                // Draw weapon and shoot
+                for (int i = 1; i <= 22; i++) {
+
+                }
+                // Holster weapon
+                for (int i = 14; i > 0; i--) {
+
+                }
             }
         }
     }
