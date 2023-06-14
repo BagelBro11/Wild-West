5ad3eedc36fe71852a1ea459cd88f8db32b36ffa
BagelBro11 <mcdougalln@limestone.on.ca>
2023-06-14 08:59:44 -0400

    Attack Features
    When in battle is false attack features go away and when it is true they appear

--- Wild West/AttackTest.java
+++ Wild West/AttackTest.java
@@ -12,20 +12,19 @@ public class AttackTest extends World
     int enemyTimer = 0;
     int heroTimer = 0;
     int backgroundTimer = 899;
-    
-    
+
     //static variable for click button
     static ClickButton click = new ClickButton();
-    
+
     //hero timer variable between two random numbers 
     int heroTimerTarget = getRandomNumber(100, 400);
-    
+
     //test
     static PlayerBattleCharacter hero = new PlayerBattleCharacter();
     static EnemyExample enemy = new EnemyExample();
-    
+
     // Battle variables
-    static boolean inBattle = false;
+    static boolean inBattle = true;
     static boolean attack = false;
     public void act(){
         enemyAttack();
@@ -37,7 +36,7 @@ public class AttackTest extends World
             addObject(new BackgroundImage(), 1350, 200);
         }
     }
-    
+
     /**
      * Constructor for objects of class AttackTest.
      * 
@@ -48,42 +47,49 @@ public class AttackTest extends World
         super(900, 400, 1, false); 
         //makes attack slider be above other objects 
         setPaintOrder(AttackSlider.class, EnemyAttack.class, HeroAttack.class, AttackBar.class, Barrier.class, HeroHealthBar.class, EnemyHealthBar.class, PlayerBattleCharacter.class, ClickButton.class, EnemyExample.class);
-        
-        addObject(new AttackBar(), 450, 325);
-        addObject(new AttackSlider(), 450, 325);
-        addObject(new Barrier(), 200, 325);
-        addObject(click, 810, 332);
+
         addObject(new HeroHealthBar(), 125, 25);
-        addObject(new EnemyHealthBar(), 775, 25);
         hero = new PlayerBattleCharacter();
-        enemy = new EnemyExample();
         addObject(new BackgroundImage(), 450, 200);
+        if(inBattle == true){
+            addObject(new AttackBar(), 450, 325);
+            addObject(new AttackSlider(), 450, 325);
+            addObject(new Barrier(), 200, 325);
+            addObject(click, 810, 332);
+            addObject(new EnemyHealthBar(), 775, 25);
+
+            enemy = new EnemyExample();
+        }
     }
-    
+
     /**
      * method which randomly controls the NPC attack 
      */
     private void enemyAttack(){
-        enemyTimer++;
-        if(enemyTimer > Greenfoot.getRandomNumber(4000)){
-            enemyTimer = 0;
-            addObject(new EnemyAttack(), 700, 325);
+        if(inBattle == true){
+            enemyTimer++;
+            if(enemyTimer > Greenfoot.getRandomNumber(4000)){
+                enemyTimer = 0;
+                addObject(new EnemyAttack(), 700, 325);
+            }
         }
     }
-    
+
     /**
      * method which randomly spawns hero attack
      */
     private void heroAttack(){
-        heroTimer++;
-        if(heroTimer == heroTimerTarget){
-            heroTimer = 0;
-            heroTimerTarget = getRandomNumber(100, 400);
-            int randX = getRandomNumber(200, 700);
-            addObject(new HeroAttack(), randX, 325);
+        if(inBattle == true){
+            heroTimer++;
+            if(heroTimer == heroTimerTarget){
+                heroTimer = 0;
+                heroTimerTarget = getRandomNumber(100, 400);
+                int randX = getRandomNumber(200, 700);
+                addObject(new HeroAttack(), randX, 325);
+            }
         }
-    }
-   
+        }
+
     /**
      * clearer random number method to specify parameters 
      */
