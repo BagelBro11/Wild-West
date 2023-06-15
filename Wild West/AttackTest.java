334485ee82bfb5f57a247de25fb1d13058acffcb
BagelBro11 <mcdougalln@limestone.on.ca>
2023-06-15 11:23:36 -0400

    small change
    changed the addition of enemy just so program would function

--- Wild West/AttackTest.java
+++ Wild West/AttackTest.java
@@ -69,7 +69,7 @@ public class AttackTest extends World
         addObject(new HeroHealthBar(), 125, 25);
         hero = new PlayerBattleCharacter();
         addObject(hero, 150, 200);
-        addObject(enemy, 1200, 200);
+        addObject(new Enemy(), 1200, 200);
         addObject(new BackgroundImage(), 450, 200);
         
     }
