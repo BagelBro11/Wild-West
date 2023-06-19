0f0b7195ca4d5b3cd5c542738f8e710f1f7960b0
SeanG428 <128709484+SeanG428@users.noreply.github.com>
2023-06-19 08:40:49 -0400

    Added more methods to Enemy class for more clarity

--- Wild West/Enemy.java
+++ Wild West/Enemy.java
@@ -44,53 +44,63 @@ public class Enemy extends Actor
     }
 
     private void animations() {
-        if (((AttackTest)getWorld()).enemyAttack == true) { // Attack
-            // imageDelay counts down to zero to change the image
-            imageDelay--;
-            if (imageDelay == 0) {
-                // Reset imageDelay
-                imageDelay = shootDelayNum;
-                if (drawGun == true) { // Draw the gun and shoot
-                    // Add one to drawAndShoot to change the image every time
-                    drawAndShoot++;
-                    if (drawAndShoot <= 22) {
-                        setImage("Enemy" + ((AttackTest)getWorld()).level + "Attack" + drawAndShoot + ".png");
-                        if (drawAndShoot == 22) {
-                            // Reset value for the next time the animation is run
-                            drawAndShoot = 0;
-                            // Set drawGun to false to run the holster animation
-                            drawGun = false;
-                        }
+        if (((AttackTest)getWorld()).enemyAttack == true) {
+            // Attack
+            attack();
+        } else {
+            // Idle
+            idle();
+        }
+    }
+
+    private void attack() {
+        // imageDelay counts down to zero to change the image
+        imageDelay--;
+        if (imageDelay == 0) {
+            // Reset imageDelay
+            imageDelay = shootDelayNum;
+            if (drawGun == true) { // Draw the gun and shoot
+                // Add one to drawAndShoot to change the image every time
+                drawAndShoot++;
+                if (drawAndShoot <= 22) {
+                    setImage("Enemy" + ((AttackTest)getWorld()).level + "Attack" + drawAndShoot + ".png");
+                    if (drawAndShoot == 22) {
+                        // Reset value for the next time the animation is run
+                        drawAndShoot = 0;
+                        // Set drawGun to false to run the holster animation
+                        drawGun = false;
                     }
-                } else { // Holster the gun
-                    if (holster > 0) {
-                        setImage("Enemy" + ((AttackTest)getWorld()).level + "Attack" + holster + ".png");
-                    }
-                    else {
-                        // Reset values and start the idle animation
-                        setImage("Enemy" + ((AttackTest)getWorld()).level + "Idle2.png");
-                        ((AttackTest)getWorld()).enemyAttack = false;
-                        drawGun = true;
-                        holster = 15;
-                    }
-                    // Change the value of holster to change what the image will be set to
-                    holster--;
                 }
+            } else { // Holster the gun
+                if (holster > 0) {
+                    setImage("Enemy" + ((AttackTest)getWorld()).level + "Attack" + holster + ".png");
+                }
+                else {
+                    // Reset values and start the idle animation
+                    setImage("Enemy" + ((AttackTest)getWorld()).level + "Idle2.png");
+                    ((AttackTest)getWorld()).enemyAttack = false;
+                    drawGun = true;
+                    holster = 15;
+                }
+                // Change the value of holster to change what the image will be set to
+                holster--;
             }
-        } else { // Idle
-            // imageDelay counts down to zero to change the image
-            imageDelay--;
-            if (imageDelay == 0) {
-                // Reset imageDelay
-                imageDelay = idleDelayNum;
-                // Add one to idle to change the image that will be set
-                idle++;
-                // Set the image
-                setImage("Enemy" + ((AttackTest)getWorld()).level + "Idle" + idle + ".png");
-                // Reset idle if it has reached its max
-                if (idle == 2){
-                    idle = 0;
-                }
+        }
+    }
+
+    private void idle() {
+        // imageDelay counts down to zero to change the image
+        imageDelay--;
+        if (imageDelay == 0) {
+            // Reset imageDelay
+            imageDelay = idleDelayNum;
+            // Add one to idle to change the image that will be set
+            idle++;
+            // Set the image
+            setImage("Enemy" + ((AttackTest)getWorld()).level + "Idle" + idle + ".png");
+            // Reset idle if it has reached its max
+            if (idle == 2){
+                idle = 0;
             }
         }
     }
