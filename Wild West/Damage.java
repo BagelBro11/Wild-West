5daf546716d24e0a6cca79d7dbfc28770ac949ae
SeanG428 <128709484+SeanG428@users.noreply.github.com>
2023-06-21 08:22:41 -0400

    Created Damage class and images

---
+++ Wild West/Damage.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Damage here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Damage extends Actor
+{
+    int count = 50;
+    public Damage(int damage) {
+        setImage("Damage(-" + damage + ").png");
+    }
+    
+    /**
+     * Act - do whatever the Damage wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move();
+        countDown();
+        delete();
+    }
+
+    private void move() {
+        setLocation(getX(), getY() - 1);
+    }
+
+    private void countDown() {
+        count--;
+    }
+
+    private void delete() {
+        if (count <= 0) {
+            getWorld().removeObject(this);
+        }
+    }
+}
